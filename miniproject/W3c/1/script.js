0cc8dc766998c54efda395edb3ee2ce2cc85c53b
uniend <dudnsgldns@naver.com>
2023-12-09 00:41:56 +0900

    ''

---
+++ miniproject/W3c/1/script.js
@@ -0,0 +1,26 @@
+
+function changeText(){
+  document.getElementById('h2').innerHTML = '변경되었습니다.'
+}
+
+
+function changeLeft(){
+  document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulbon.gif'
+}
+function changeRight(){
+  document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulboff.gif'
+}
+
+
+function changeStyle(){
+  document.getElementById('styleChange').style.fontSize = '36px'
+}
+
+function changeDisplay(){
+  document.getElementById('displayChange').style.display ='none'
+  
+}
+
+function changeDisplayBlock(){
+  document.getElementById('changeDisplayBlock').style.display = 'block'
+}
